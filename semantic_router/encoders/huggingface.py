42dea8dd0b1fe5f331abe602b043f2bd97f0f543
James Briggs <35938317+jamescalam@users.noreply.github.com>
2023-12-02 14:03:55 -0800

    update tests and removed WIP modules/objects

--- semantic_router/encoders/huggingface.py
+++
@@ -1,9 +0,0 @@
-from semantic_router.encoders import BaseEncoder
-
-
-class HuggingFaceEncoder(BaseEncoder):
-    def __init__(self, name: str):
-        self.name = name
-
-    def __call__(self, docs: list[str]) -> list[float]:
-        raise NotImplementedError
---
+++ tests/unit/encoders/test_bm25.py
@@ -0,0 +1,21 @@
+import pytest
+
+from semantic_router.encoders import BM25Encoder
+
+
+@pytest.fixture
+def bm25_encoder():
+    return BM25Encoder()
+
+
+class TestBM25Encoder:
+    def test_initialization(self):
+        self.bm25_encoder = BM25Encoder()
+        assert len(self.bm25_encoder.idx_mapping) != 0
+
+    def test_call_method(self):
+        result = self.bm25_encoder(["test"])
+        assert isinstance(result, list), "Result should be a list"
+        assert all(
+            isinstance(sublist, list) for sublist in result
+        ), "Each item in result should be a list"
