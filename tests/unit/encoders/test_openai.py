8a3fb3b818117adc5d4799bc394d78bb398d93cb
Simonas <20096648+simjak@users.noreply.github.com>
2023-12-13 18:32:15 +0200

    bump openai version to ^1.0.0

--- tests/unit/encoders/test_openai.py
+++ tests/unit/encoders/test_openai.py
@@ -1,122 +1,84 @@
-import openai
 import pytest
-from openai.error import RateLimitError
+from openai import OpenAIError
 
 from semantic_router.encoders import OpenAIEncoder
 
 
 @pytest.fixture
 def openai_encoder(mocker):
-    mocker.patch("openai.Embedding.create")
-    return OpenAIEncoder(name="test-engine", openai_api_key="test_api_key")
+    mocker.patch("openai.Client")
+    return OpenAIEncoder(openai_api_key="test_api_key")
 
 
 class TestOpenAIEncoder:
-    def test_initialization_with_api_key(self, openai_encoder):
-        assert openai.api_key == "test_api_key", "API key should be set correctly"
-        assert openai_encoder.name == "test-engine", "Engine name not set correctly"
+    def test_openai_encoder_init_success(self, mocker):
+        mocker.patch("os.getenv", return_value="fake-api-key")
+        encoder = OpenAIEncoder()
+        assert encoder.client is not None
 
-    def test_initialization_without_api_key(self, mocker, monkeypatch):
-        monkeypatch.delenv("OPENAI_API_KEY", raising=False)
-        mocker.patch("openai.Embedding.create")
-        with pytest.raises(ValueError):
-            OpenAIEncoder(name="test-engine")
+    def test_openai_encoder_init_no_api_key(self, mocker):
+        mocker.patch("os.getenv", return_value=None)
+        with pytest.raises(ValueError) as e:
+            OpenAIEncoder()
+        assert "OpenAI API key cannot be 'None'." in str(e.value)
 
-    def test_call_method_success(self, openai_encoder, mocker):
-        mocker.patch(
-            "openai.Embedding.create",
-            return_value={"data": [{"embedding": [0.1, 0.2, 0.3]}]},
+    def test_openai_encoder_call_uninitialized_client(self, openai_encoder):
+        # Set the client to None to simulate an uninitialized client
+        openai_encoder.client = None
+        with pytest.raises(ValueError) as e:
+            openai_encoder(["test document"])
+        assert "OpenAI client is not initialized." in str(e.value)
+
+    def test_openai_encoder_init_exception(self, mocker):
+        mocker.patch("os.getenv", return_value="fake-api-key")
+        mocker.patch("openai.Client", side_effect=Exception("Initialization error"))
+        with pytest.raises(ValueError) as e:
+            OpenAIEncoder()
+        assert (
+            "OpenAI API client failed to initialize. Error: Initialization error"
+            in str(e.value)
         )
 
-        result = openai_encoder(["test"])
-        assert isinstance(result, list), "Result should be a list"
-        assert len(result) == 1 and len(result[0]) == 3, "Result list size is incorrect"
-
-    def test_call_method_rate_limit_error__raises_value_error_after_max_retries(
-        self, openai_encoder, mocker
-    ):
-        mocker.patch("semantic_router.encoders.openai.sleep")
-        mocker.patch(
-            "openai.Embedding.create",
-            side_effect=RateLimitError(message="rate limit exceeded", http_status=429),
+    def test_openai_encoder_call_success(self, openai_encoder, mocker):
+        mocker.patch("os.getenv", return_value="fake-api-key")
+        mocker.patch.object(
+            openai_encoder.client.embeddings,
+            "create",
+            return_value={"data": [{"embedding": [0.1, 0.2]}]},
         )
+        embeddings = openai_encoder(["test document"])
+        assert embeddings == [[0.1, 0.2]]
 
-        with pytest.raises(ValueError):
-            openai_encoder(["test"])
-
-    def test_call_method_failure(self, openai_encoder, mocker):
-        mocker.patch("openai.Embedding.create", return_value={})
-
-        with pytest.raises(ValueError):
-            openai_encoder(["test"])
-
-    def test_call_method_rate_limit_error__exponential_backoff_single_retry(
-        self, openai_encoder, mocker
-    ):
-        mock_sleep = mocker.patch("semantic_router.encoders.openai.sleep")
-        mocker.patch(
-            "openai.Embedding.create",
-            side_effect=[
-                RateLimitError("rate limit exceeded"),
-                {"data": [{"embedding": [1, 2, 3]}]},
-            ],
+    def test_openai_encoder_call_with_retries(self, openai_encoder, mocker):
+        mocker.patch("os.getenv", return_value="fake-api-key")
+        mocker.patch("time.sleep", return_value=None)  # To speed up the test
+        mocker.patch.object(
+            openai_encoder.client.embeddings,
+            "create",
+            side_effect=OpenAIError("Test error"),
         )
+        with pytest.raises(ValueError) as e:
+            openai_encoder(["test document"])
+        assert "No embeddings returned. Error: Test error" in str(e.value)
 
-        openai_encoder(["sample text"])
-
-        mock_sleep.assert_called_once_with(1)  # 2**0
-
-    def test_call_method_rate_limit_error__exponential_backoff_multiple_retries(
-        self, openai_encoder, mocker
-    ):
-        mock_sleep = mocker.patch("semantic_router.encoders.openai.sleep")
-        mocker.patch(
-            "openai.Embedding.create",
-            side_effect=[
-                RateLimitError("rate limit exceeded"),
-                RateLimitError("rate limit exceeded"),
-                {"data": [{"embedding": [1, 2, 3]}]},
-            ],
+    def test_openai_encoder_call_failure_non_openai_error(self, openai_encoder, mocker):
+        mocker.patch("os.getenv", return_value="fake-api-key")
+        mocker.patch("time.sleep", return_value=None)  # To speed up the test
+        mocker.patch.object(
+            openai_encoder.client.embeddings,
+            "create",
+            side_effect=Exception("Non-OpenAIError"),
         )
+        with pytest.raises(ValueError) as e:
+            openai_encoder(["test document"])
+        assert "OpenAI API call failed. Error: Non-OpenAIError" in str(e.value)
 
-        openai_encoder(["sample text"])
-
-        assert mock_sleep.call_count == 2
-        mock_sleep.assert_any_call(1)  # 2**0
-        mock_sleep.assert_any_call(2)  # 2**1
-
-    def test_call_method_rate_limit_error__exponential_backoff_max_retries_exceeded(
-        self, openai_encoder, mocker
-    ):
-        mock_sleep = mocker.patch("semantic_router.encoders.openai.sleep")
-        mocker.patch(
-            "openai.Embedding.create", side_effect=RateLimitError("rate limit exceeded")
+    def test_openai_encoder_call_successful_retry(self, openai_encoder, mocker):
+        mocker.patch("os.getenv", return_value="fake-api-key")
+        mocker.patch("time.sleep", return_value=None)  # To speed up the test
+        responses = [OpenAIError("Test error"), {"data": [{"embedding": [0.1, 0.2]}]}]
+        mocker.patch.object(
+            openai_encoder.client.embeddings, "create", side_effect=responses
         )
-
-        with pytest.raises(ValueError):
-            openai_encoder(["sample text"])
-
-        assert mock_sleep.call_count == 5  # Assuming 5 retries
-        mock_sleep.assert_any_call(1)  # 2**0
-        mock_sleep.assert_any_call(2)  # 2**1
-        mock_sleep.assert_any_call(4)  # 2**2
-        mock_sleep.assert_any_call(8)  # 2**3
-        mock_sleep.assert_any_call(16)  # 2**4
-
-    def test_call_method_rate_limit_error__exponential_backoff_successful(
-        self, openai_encoder, mocker
-    ):
-        mock_sleep = mocker.patch("semantic_router.encoders.openai.sleep")
-        mocker.patch(
-            "openai.Embedding.create",
-            side_effect=[
-                RateLimitError("rate limit exceeded"),
-                RateLimitError("rate limit exceeded"),
-                {"data": [{"embedding": [1, 2, 3]}]},
-            ],
-        )
-
-        embeddings = openai_encoder(["sample text"])
-
-        assert mock_sleep.call_count == 2
-        assert embeddings == [[1, 2, 3]]
+        embeddings = openai_encoder(["test document"])
+        assert embeddings == [[0.1, 0.2]]
