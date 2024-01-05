7d9cc0aef4e02b164adb7ba5b2b2d0ce65b93c62
Simonas <20096648+simjak@users.noreply.github.com>
2024-01-05 09:26:05 +0200

    fix: conventional commits for PR titles

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     extends: ['@commitlint/config-conventional'],
     rules: {
-        'subject-case': [2, 'always', 'sentence-case'],
+        'subject-case': [0, 'never'],
         'subject-max-length': [2, 'always', 60],
         'type-enum': [2, 'always', ['build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'style', 'test']],
     },
