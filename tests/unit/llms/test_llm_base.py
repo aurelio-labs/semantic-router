dcea69aa2fefc9d5ba7f49cd7cedc01b1558f343
Bogdan Buduroiu <bogdan@buduroiu.com>
2024-01-13 15:06:26 +0800

    Adds BaseLLM tests for valid input

--- tests/unit/llms/test_llm_base.py
+++ tests/unit/llms/test_llm_base.py
@@ -14,3 +14,26 @@ class TestBaseLLM:
     def test_base_llm_call_method_not_implemented(self, base_llm):
         with pytest.raises(NotImplementedError):
             base_llm("test")
+
+    def test_base_llm_is_valid_inputs_valid_input_pass(self, base_llm):
+        test_schema = {
+            "name": "get_time",
+            "description": 'Finds the current time in a specific timezone.\n\n:param timezone: The timezone to find the current time in, should\n    be a valid timezone from the IANA Time Zone Database like\n    "America/New_York" or "Europe/London". Do NOT put the place\n    name itself like "rome", or "new york", you must provide\n    the IANA format.\n:type timezone: str\n:return: The current time in the specified timezone.',
+            "signature": "(timezone: str) -> str",
+            "output": "<class 'str'>",
+        }
+        test_inputs = {"timezone": "America/New_York"}
+
+        assert base_llm._is_valid_inputs(test_inputs, test_schema) is True
+
+    @pytest.mark.skip(reason="TODO: bug in is_valid_inputs")
+    def test_base_llm_is_valid_inputs_valid_input_fail(self, base_llm):
+        test_schema = {
+            "name": "get_time",
+            "description": 'Finds the current time in a specific timezone.\n\n:param timezone: The timezone to find the current time in, should\n    be a valid timezone from the IANA Time Zone Database like\n    "America/New_York" or "Europe/London". Do NOT put the place\n    name itself like "rome", or "new york", you must provide\n    the IANA format.\n:type timezone: str\n:return: The current time in the specified timezone.',
+            "signature": "(timezone: str) -> str",
+            "output": "<class 'str'>",
+        }
+        test_inputs = {"timezone": None}
+
+        assert base_llm._is_valid_inputs(test_inputs, test_schema) is False
