dfb7571fba9abdd2700eaa15b7e94dd5fbbfc411
James Briggs <35938317+jamescalam@users.noreply.github.com>
2024-01-23 10:44:44 +0100

    fix AzureOpenAILLM import

--- semantic_router/llms/__init__.py
+++ semantic_router/llms/__init__.py
@@ -4,4 +4,4 @@ from semantic_router.llms.openai import OpenAILLM
 from semantic_router.llms.openrouter import OpenRouterLLM
 from semantic_router.llms.zure import AzureOpenAILLM
 
-__all__ = ["BaseLLM", "OpenAILLM", "OpenRouterLLM", "CohereLLM"]
+__all__ = ["BaseLLM", "OpenAILLM", "OpenRouterLLM", "CohereLLM", "AzureOpenAILLM"]
