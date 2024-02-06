b0d164c79f984391ee0339cb524b2753496e5ab1
Siraj R Aizlewood <siraj@aurelio.ai>
2024-02-06 23:40:00 +0400

    Added Explanation about Base Class Being a Placeholder

--- semantic_router/indices/base.py
+++ semantic_router/indices/base.py
@@ -1,4 +1,5 @@
 from pydantic.v1 import BaseModel
 
 class BaseIndex(BaseModel):
+    # Currently just a placedholder until more indexing methods are added and common attributes/methods are identified.
     pass
