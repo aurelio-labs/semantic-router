97007ca3465919bbf57245c129315a0cc5aaefd1
zahid-syed <zahid.s2618@gmail.com>
2024-03-13 11:53:57 -0400

    unable to fix coverage for both MistralEncoder and MistralLLM

--- tests/unit/llms/test_llm_llamacpp.py
+++ tests/unit/llms/test_llm_llamacpp.py
@@ -15,7 +15,6 @@ def llamacpp_llm(mocker):
 
 
 class TestLlamaCppLLM:
-
     def test_llama_cpp_import_errors(self, llamacpp_llm):
         with patch.dict("sys.modules", {"llama_cpp": None}):
             with pytest.raises(ImportError) as error:
