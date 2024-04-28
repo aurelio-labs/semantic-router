516f941868beb8b8fc6d5e2f004f13c7e317a504
James Briggs <james.briggs@hotmail.com>
2024-04-28 16:41:47 +0800

    fix: adjust filepath

--- tests/integration/encoders/test_openai_integration.py
+++ tests/integration/encoders/test_openai_integration.py
@@ -3,7 +3,7 @@ import pytest
 
 from semantic_router.encoders import OpenAIEncoder
 
-with open("tests/integration/encoders/57640.4032.txt", "r") as fp:
+with open("tests/integration/57640.4032.txt", "r") as fp:
     long_doc = fp.read()
 
 
