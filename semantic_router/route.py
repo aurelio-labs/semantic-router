f59ee3c011996391b37de82257606cd2b98fb90e
Siraj R Aizlewood <siraj@aurelio.ai>
2024-04-30 01:58:14 +0400

    Simplified __call__ in route.py as we don't have 2 separate methods of treating dynamic routes anymore.

--- semantic_router/route.py
+++ semantic_router/route.py
@@ -57,37 +57,22 @@ class Route(BaseModel):
         arbitrary_types_allowed = True
 
     def __call__(self, query: Optional[str] = None) -> RouteChoice:
-        if self.function_schema and self.openai_function_schema:
-            raise ValueError(
-                "Both function_schema and openai_function_schema cannot be provided. Please provide only one."
-            )
-        if self.function_schema or self.openai_function_schema:
+        if self.function_schema:
             if not self.llm:
                 raise ValueError(
                     "LLM is required for dynamic routes. Please ensure the `llm` "
                     "attribute is set."
                 )
-            if query is None or not isinstance(query, str):
+            elif query is None:
                 raise ValueError(
                     "Query is required for dynamic routes. Please ensure the `query` "
                     "argument is passed."
                 )
-            if self.function_schema:
-                extracted_inputs = self.llm.extract_function_inputs(
-                    query=query, function_schema=self.function_schema
-                )
-                func_call = extracted_inputs
-            elif (
-                self.openai_function_schema
-            ):  # Logically must be self.openai_function_schema, but keeps MyPy happy.
-                if not isinstance(self.llm, OpenAILLM):
-                    raise TypeError(
-                        "LLM must be an instance of OpenAILLM for openai_function_schema."
-                    )
-                extracted_inputs = self.llm.extract_function_inputs_openai(
-                    query=query, openai_function_schema=self.openai_function_schema
-                )
-                func_call = extracted_inputs
+            # if a function schema is provided we generate the inputs
+            extracted_inputs = self.llm.extract_function_inputs(
+                query=query, function_schema=self.function_schema
+            )
+            func_call = extracted_inputs
         else:
             # otherwise we just pass None for the call
             func_call = None
