21dd2fdf31c1fe46df6239ceeb415f891a336ea4
Siraj R Aizlewood <siraj@aurelio.ai>
2024-05-13 18:10:44 +0400

    Linting.

--- tests/unit/llms/test_llm_base.py
+++ tests/unit/llms/test_llm_base.py
@@ -89,7 +89,9 @@ class TestBaseLLM:
 
     def test_mandatory_args_only(self, base_llm, mixed_function_schema):
         inputs = {"mandatory1": "value1", "mandatory2": 42}
-        assert base_llm._is_valid_inputs(inputs, mixed_function_schema)  # True is implied
+        assert base_llm._is_valid_inputs(
+            inputs, mixed_function_schema
+        )  # True is implied
 
     def test_all_args_provided(self, base_llm, mixed_function_schema):
         inputs = {
@@ -98,7 +100,9 @@ class TestBaseLLM:
             "optional1": "opt1",
             "optional2": "opt2",
         }
-        assert base_llm._is_valid_inputs(inputs, mixed_function_schema)  # True is implied
+        assert base_llm._is_valid_inputs(
+            inputs, mixed_function_schema
+        )  # True is implied
 
     def test_missing_mandatory_arg(self, base_llm, mixed_function_schema):
         inputs = {"mandatory1": "value1", "optional1": "opt1", "optional2": "opt2"}
@@ -116,7 +120,9 @@ class TestBaseLLM:
 
     def test_check_for_mandatory_inputs_all_present(self, base_llm, mandatory_params):
         inputs = {"param1": "value1", "param2": "value2"}
-        assert base_llm._check_for_mandatory_inputs(inputs, mandatory_params)  # True is implied
+        assert base_llm._check_for_mandatory_inputs(
+            inputs, mandatory_params
+        )  # True is implied
 
     def test_check_for_mandatory_inputs_missing_one(self, base_llm, mandatory_params):
         inputs = {"param1": "value1"}
