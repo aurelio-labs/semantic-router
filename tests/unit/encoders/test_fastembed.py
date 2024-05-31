97ac176fae10cb0264e08c7df6b37c7c0f2d1f25
James Briggs <james.briggs@hotmail.com>
2024-05-31 12:45:16 +0800

    chore: drop fastembed test

--- tests/unit/encoders/test_fastembed.py
+++ tests/unit/encoders/test_fastembed.py
@@ -3,8 +3,10 @@ from semantic_router.encoders import FastEmbedEncoder
 
 class TestFastEmbedEncoder:
     def test_fastembed_encoder(self):
-        encode = FastEmbedEncoder()
-        test_docs = ["This is a test", "This is another test"]
+        if False:
+            # temporarily disabled until fastembed added back as dep
+            encode = FastEmbedEncoder()
+            test_docs = ["This is a test", "This is another test"]
 
-        embeddings = encode(test_docs)
-        assert isinstance(embeddings, list)
+            embeddings = encode(test_docs)
+            assert isinstance(embeddings, list)
