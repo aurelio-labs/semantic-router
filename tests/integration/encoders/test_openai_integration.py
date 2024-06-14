f8ead69913c6de4bf8e293dbd307ecc024bde30f
James Briggs <james.briggs@hotmail.com>
2024-06-14 16:11:40 +0800

    fix: add skips to pinecone openai tests

--- tests/integration/encoders/test_openai_integration.py
+++ tests/integration/encoders/test_openai_integration.py
@@ -1,3 +1,4 @@
+import os
 import pytest
 from semantic_router.encoders.openai import OpenAIEncoder
 
@@ -11,22 +12,37 @@ def openai_encoder():
 
 
 class TestOpenAIEncoder:
+    @pytest.mark.skipif(
+        os.environ.get("OPENAI_API_KEY") is None, reason="OpenAI API key required"
+    )
     def test_openai_encoder_init_success(self, openai_encoder):
         assert openai_encoder.client is not None
 
+    @pytest.mark.skipif(
+        os.environ.get("OPENAI_API_KEY") is None, reason="OpenAI API key required"
+    )
     def test_openai_encoder_dims(self, openai_encoder):
         embeddings = openai_encoder(["test document"])
         assert len(embeddings) == 1
         assert len(embeddings[0]) == 1536
 
+    @pytest.mark.skipif(
+        os.environ.get("OPENAI_API_KEY") is None, reason="OpenAI API key required"
+    )
     def test_openai_encoder_call_truncation(self, openai_encoder):
         openai_encoder([long_doc])
 
+    @pytest.mark.skipif(
+        os.environ.get("OPENAI_API_KEY") is None, reason="OpenAI API key required"
+    )
     def test_openai_encoder_call_no_truncation(self, openai_encoder):
         with pytest.raises(ValueError) as _:
             # default truncation is True
             openai_encoder([long_doc], truncate=False)
 
+    @pytest.mark.skipif(
+        os.environ.get("OPENAI_API_KEY") is None, reason="OpenAI API key required"
+    )
     def test_openai_encoder_call_uninitialized_client(self, openai_encoder):
         # Set the client to None to simulate an uninitialized client
         openai_encoder.client = None
