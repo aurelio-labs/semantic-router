efb310f141f5fb1b864245ef87543f52473532d6
Yara97Mansour <165589291+Yara97Mansour@users.noreply.github.com>
2024-08-21 14:52:38 +0300

    Update __init__.py

--- semantic_router/llms/__init__.py
+++ semantic_router/llms/__init__.py
@@ -4,6 +4,7 @@ from semantic_router.llms.llamacpp import LlamaCppLLM
 from semantic_router.llms.mistral import MistralAILLM
 from semantic_router.llms.openai import OpenAILLM
 from semantic_router.llms.openrouter import OpenRouterLLM
+from semantic_router.llms.unify import UnifyLLM
 from semantic_router.llms.zure import AzureOpenAILLM
 
 __all__ = [
@@ -14,4 +15,5 @@ __all__ = [
     "CohereLLM",
     "AzureOpenAILLM",
     "MistralAILLM",
+    "UnifyLLM",
 ]
