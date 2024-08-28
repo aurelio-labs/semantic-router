7f1f3bf79af2f544fa3a929c5c04be2faba79544
Kacper-W-Kozdon <k.kozdon@gmail.com>
2024-08-28 13:33:21 +0200

    pytest mock passing 7/7

--- tests/unit/llms/test_llm_unify.py
+++ tests/unit/llms/test_llm_unify.py
@@ -52,31 +52,27 @@ class TestUnifyLLM:
             unify_llm(llm_input)
         assert "Unify client is not initialized." in str(e.value)
 
-
-# @pytest.fixture
-# def unify_llm():
-#     return UnifyLLM()
-
-
-# class TestUnifyLLM:
-#     def test_unify_llm_init_success(self, unify_llm):
-#         assert unify_llm.name == "gpt-4o@openai"
-#         assert unify_llm.temperature == 0.01
-#         assert unify_llm.max_tokens == 200
-#         assert unify_llm.stream is False
-
-#     def test_unify_llm_call_success(self, unify_llm, mocker):
-
-#         mock_response = mocker.MagicMock()
-#         mock_response.json.return_value = {"message": {"content": "test response"}}
-#         mocker.patch.object(unify_llm.client, "generate", return_value=mock_response)
-
-#         output = unify_llm([Message(role="user", content="test")])
-#         assert output == "test response"
-
     def test_unify_llm_error_handling(self, unify_llm, mocker):
 
         mocker.patch.object(unify_llm.client, "generate", side_effect=Exception("LLM error"))
         with pytest.raises(UnifyError) as exc_info:
             unify_llm([Message(role="user", content="test")])
         assert "LLM error" in f"{str(exc_info)}, {str(exc_info.value)}"
+        
+
+    def test_unify_llm_call_success(self, unify_llm, mocker):
+
+        # mock_response = mocker.MagicMock()
+        # mock_response.choices[0].message.content = {"message": {"content": "test response"}}
+        mock_response = "test response"  # unify currently outputs strings in generate, not completions
+        mocker.patch.object(unify_llm.client, "generate", return_value=mock_response)
+
+        output = unify_llm([Message(role="user", content="test")])
+        assert output == "test response"
+
+
+    # def test_unify_llm_init_success(self, unify_llm):
+    #     assert unify_llm.name == "gpt-4o@openai"
+    #     assert unify_llm.temperature == 0.01
+    #     assert unify_llm.max_tokens == 200
+    #     assert unify_llm.stream is False
