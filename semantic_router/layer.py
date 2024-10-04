ea7d3fb93dadb7994750efc44360f03a8d246690
Vittorio <vittorio.mayellaro.dev@gmail.com>
2024-10-04 13:59:09 +0200

    Implemented is_synced for Pinecone Index and tested it

--- semantic_router/layer.py
+++ semantic_router/layer.py
@@ -526,6 +526,15 @@ class RouteLayer:
             logger.error(f"Failed to add routes to the index: {e}")
             raise Exception("Indexing error occurred") from e
 
+    def is_synced(self) -> bool:
+        if not self.index.sync:
+            raise ValueError("Index is not set to sync with remote index.")
+        
+        local_route_names, local_utterances, local_function_schemas, local_metadata = (
+            self._extract_routes_details(self.routes, include_metadata=True)
+        )
+        return self.index.is_synced(local_route_names, local_utterances, local_function_schemas, local_metadata)
+                
     def _add_and_sync_routes(self, routes: List[Route]):
         # create embeddings for all routes and sync at startup with remote ones based on sync setting
         local_route_names, local_utterances, local_function_schemas, local_metadata = (
