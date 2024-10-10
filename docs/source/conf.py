d307fd2b296cfd22f3443421bee39b9988cec14b
James Briggs <james.briggs@hotmail.com>
2024-10-10 23:11:34 +0200

    chore: v0.0.72

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,7 @@ sys.path.insert(0, os.path.abspath("../.."))  # Source code dir relative to this
 project = "Semantic Router"
 copyright = "2024, Aurelio AI"
 author = "Aurelio AI"
-release = "0.0.71"
+release = "0.0.72"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
