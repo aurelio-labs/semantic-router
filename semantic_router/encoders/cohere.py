098de29b231e427bbb1ede10e63edc2bd801e7f3
James Briggs <james.briggs@hotmail.com>
2024-10-10 23:00:35 +0200

    feat: move cohere to optional dep

--- semantic_router/encoders/cohere.py
+++ semantic_router/encoders/cohere.py
@@ -1,15 +1,14 @@
 import os
-from typing import List, Optional
+from typing import Any, List, Optional
 
-import cohere
-from cohere.types.embed_response import EmbeddingsByTypeEmbedResponse
+from pydantic.v1 import PrivateAttr
 
 from semantic_router.encoders import BaseEncoder
 from semantic_router.utils.defaults import EncoderDefault
 
 
 class CohereEncoder(BaseEncoder):
-    client: Optional[cohere.Client] = None
+    client: Any = PrivateAttr()
     type: str = "cohere"
     input_type: Optional[str] = "search_query"
 
@@ -28,15 +27,36 @@ class CohereEncoder(BaseEncoder):
             input_type=input_type,  # type: ignore
         )
         self.input_type = input_type
+        self.client = self._initialize_client(cohere_api_key)
+    
+    def _initialize_client(self, cohere_api_key: Optional[str] = None):
+        """Initializes the Cohere client.
+
+        :param cohere_api_key: The API key for the Cohere client, can also
+        be set via the COHERE_API_KEY environment variable.
+
+        :return: An instance of the Cohere client.
+        """
+        try:
+            import cohere
+            from cohere.types.embed_response import EmbeddingsByTypeEmbedResponse
+            self.EmbeddingsByTypeEmbedResponse = EmbeddingsByTypeEmbedResponse
+        except ImportError:
+            raise ImportError(
+                "Please install Cohere to use CohereEncoder. "
+                "You can install it with: "
+                "`pip install 'semantic-router[cohere]'`"
+            )
         cohere_api_key = cohere_api_key or os.getenv("COHERE_API_KEY")
         if cohere_api_key is None:
             raise ValueError("Cohere API key cannot be 'None'.")
         try:
-            self.client = cohere.Client(cohere_api_key)
+            client = cohere.Client(cohere_api_key)
         except Exception as e:
             raise ValueError(
                 f"Cohere API client failed to initialize. Error: {e}"
             ) from e
+        return client
 
     def __call__(self, docs: List[str]) -> List[List[float]]:
         if self.client is None:
@@ -46,7 +66,7 @@ class CohereEncoder(BaseEncoder):
                 texts=docs, input_type=self.input_type, model=self.name
             )
             # Check for unsupported type.
-            if isinstance(embeds, EmbeddingsByTypeEmbedResponse):
+            if isinstance(embeds, self.EmbeddingsByTypeEmbedResponse):
                 raise NotImplementedError(
                     "Handling of EmbedByTypeResponseEmbeddings is not implemented."
                 )
