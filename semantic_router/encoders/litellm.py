75553d38346cf6d4d406dd6a7869b6e35ed5322b
James Briggs <35938317+jamescalam@users.noreply.github.com>
2025-04-07 23:37:04 +0400

    feat: add asym mixin to litellm encoder

--- semantic_router/encoders/litellm.py
+++ semantic_router/encoders/litellm.py
@@ -1,8 +1,10 @@
+import os
 from typing import Any
 
 import litellm
 
 from semantic_router.encoders import DenseEncoder
+from semantic_router.encoders.base import AsymmetricDenseMixin
 from semantic_router.utils.defaults import EncoderDefault
 
 
@@ -21,7 +23,7 @@ def litellm_to_list(embeds: litellm.EmbeddingResponse) -> list[list[float]]:
     return [x["embedding"] for x in embeds.data]
 
 
-class LiteLLMEncoder(DenseEncoder):
+class LiteLLMEncoder(DenseEncoder, AsymmetricDenseMixin):
     """LiteLLM encoder class for generating embeddings using LiteLLM.
 
     The LiteLLMEncoder class is a subclass of DenseEncoder and utilizes the LiteLLM SDK
@@ -35,50 +37,79 @@ class LiteLLMEncoder(DenseEncoder):
         self,
         name: str | None = None,
         score_threshold: float | None = None,
+        api_key: str | None = None,
     ):
-        """Initialize the OpenAIEncoder.
+        """Initialize the LiteLLMEncoder.
 
-        :param name: The name of the embedding model to use.
+        :param name: The name of the embedding model to use. Must use LiteLLM naming
+            convention (e.g. "openai/text-embedding-3-small" or "mistral/mistral-embed").
         :type name: str
         :param score_threshold: The score threshold for the embeddings.
         :type score_threshold: float
         """
         if name is None:
-            self.name = "openai/" + EncoderDefault.OPENAI.value["embedding_model"]
-        else:
-            self.name = name
-        if score_threshold is None:
-            self.score_threshold = 0.3
-        else:
-            self.score_threshold = score_threshold
+            # defaults to default openai model if none provided
+            name = "openai/" + EncoderDefault.OPENAI.value["embedding_model"]
+        super().__init__(
+            name=name,
+            score_threshold=score_threshold if score_threshold is not None else 0.3,
+        )
+        self.type, self.name = self.name.split("/")
+        if api_key is None:
+            api_key = os.getenv(self.type.upper() + "_API_KEY")
+        if api_key is None:
+            raise ValueError(
+                "Expected API key via `api_key` parameter or `{self.type.upper()}_API_KEY` "
+                "environment variable."
+            )
+        os.environ[self.type.upper() + "_API_KEY"] = api_key
 
     def __call__(self, docs: list[Any], **kwargs) -> list[list[float]]:
         """Encode a list of text documents into embeddings using LiteLLM.
 
         :param docs: List of text documents to encode.
         :return: List of embeddings for each document."""
-        embeds = litellm.embedding(input=docs, model=self.name, **kwargs)
-        if (
-            not embeds
-            or not isinstance(embeds, litellm.EmbeddingResponse)
-            or not embeds.data
-        ):
-            raise ValueError("No embeddings returned")
-        embeddings = [x["embedding"] for x in embeds.data]
-        return embeddings
+        return self.encode_queries(docs, **kwargs)
 
     async def acall(self, docs: list[Any], **kwargs) -> list[list[float]]:
         """Encode a list of documents into embeddings using LiteLLM asynchronously.
 
         :param docs: List of documents to encode.
         :return: List of embeddings for each document."""
-        embeds = await litellm.aembedding(input=docs, model=self.name, **kwargs)
-        if (
-            not embeds
-            or not isinstance(embeds, litellm.EmbeddingResponse)
-            or not embeds.data
-        ):
-            raise ValueError("No embeddings returned.")
+        return await self.aencode_queries(docs, **kwargs)
 
-        embeddings = [x["embedding"] for x in embeds.data]
-        return embeddings
+    def encode_queries(self, docs: list[str], **kwargs) -> list[list[float]]:
+        try:
+            embeds = litellm.embedding(
+                input=docs, model=f"{self.type}/{self.name}", **kwargs
+            )
+            return litellm_to_list(embeds)
+        except Exception as e:
+            raise ValueError(f"{self.type.capitalize()} API call failed. Error: {e}") from e
+
+    def encode_documents(self, docs: list[str], **kwargs) -> list[list[float]]:
+        try:
+            embeds = litellm.embedding(
+                input=docs, model=f"{self.type}/{self.name}", **kwargs
+            )
+            return litellm_to_list(embeds)
+        except Exception as e:
+            raise ValueError(f"{self.type.capitalize()} API call failed. Error: {e}") from e
+
+    async def aencode_queries(self, docs: list[str], **kwargs) -> list[list[float]]:
+        try:
+            embeds = await litellm.aembedding(
+                input=docs, model=f"{self.type}/{self.name}", **kwargs
+            )
+            return litellm_to_list(embeds)
+        except Exception as e:
+            raise ValueError(f"{self.type.capitalize()} API call failed. Error: {e}") from e
+
+    async def aencode_documents(self, docs: list[str], **kwargs) -> list[list[float]]:
+        try:
+            embeds = await litellm.aembedding(
+                input=docs, model=f"{self.type}/{self.name}", **kwargs
+            )
+            return litellm_to_list(embeds)
+        except Exception as e:
+            raise ValueError(f"{self.type.capitalize()} API call failed. Error: {e}") from e
