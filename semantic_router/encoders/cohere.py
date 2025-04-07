02acb81dadae55332f2e67d70cda905d3c3c5228
James Briggs <35938317+jamescalam@users.noreply.github.com>
2025-04-07 23:51:06 +0400

    feat: abstract away most methods from cohere and mistral

--- semantic_router/encoders/cohere.py
+++ semantic_router/encoders/cohere.py
@@ -5,20 +5,19 @@ import litellm
 from pydantic import PrivateAttr
 from typing_extensions import deprecated
 
-from semantic_router.encoders import DenseEncoder
 from semantic_router.encoders.base import AsymmetricDenseMixin
-from semantic_router.encoders.litellm import litellm_to_list
+from semantic_router.encoders.litellm import LiteLLMEncoder, litellm_to_list
 from semantic_router.utils.defaults import EncoderDefault
 
 
-class CohereEncoder(DenseEncoder, AsymmetricDenseMixin):
+class CohereEncoder(LiteLLMEncoder):
     """Dense encoder that uses Cohere API to embed documents. Supports text only. Requires
     a Cohere API key from https://dashboard.cohere.com/api-keys.
     """
 
-    _client: Any = PrivateAttr()
-    _async_client: Any = PrivateAttr()
-    _embed_type: Any = PrivateAttr()
+    _client: Any = PrivateAttr()  # TODO: deprecated, to remove in v0.2.0
+    _async_client: Any = PrivateAttr()  # TODO: deprecated, to remove in v0.2.0
+    _embed_type: Any = PrivateAttr()  # TODO: deprecated, to remove in v0.2.0
     type: str = "cohere"
 
     def __init__(
@@ -29,32 +28,29 @@ class CohereEncoder(DenseEncoder, AsymmetricDenseMixin):
     ):
         """Initialize the Cohere encoder.
 
-        :param name: The name of the embedding model to use.
+        :param name: The name of the embedding model to use such as "embed-english-v3.0" or
+            "embed-multilingual-v3.0".
         :type name: str
         :param cohere_api_key: The API key for the Cohere client, can also
             be set via the COHERE_API_KEY environment variable.
         :type cohere_api_key: str
         :param score_threshold: The threshold for the score of the embedding.
         :type score_threshold: float
-        :param input_type: The type of input to embed.
-        :type input_type: str
         """
+        # get default model name if none provided and convert to litellm format
         if name is None:
-            name = EncoderDefault.COHERE.value["embedding_model"]
+            name = f"cohere/{EncoderDefault.COHERE.value['embedding_model']}"
+        elif not name.startswith("cohere/"):
+            name = f"cohere/{name}"
         super().__init__(
             name=name,
             score_threshold=score_threshold,
+            api_key=cohere_api_key,
         )
-        if cohere_api_key is None:
-            cohere_api_key = os.getenv("COHERE_API_KEY")
-        if cohere_api_key is None:
-            raise ValueError(
-                "Cohere API key must be provided via `cohere_api_key` parameter or "
-                "`COHERE_API_KEY` environment variable."
-            )
-        self._client = None
-        self._async_client = None
+        self._client = None  # TODO: deprecated, to remove in v0.2.0
+        self._async_client = None  # TODO: deprecated, to remove in v0.2.0
 
+    # TODO: deprecated, to remove in v0.2.0
     @deprecated("_initialize_client method no longer required")
     def _initialize_client(self, cohere_api_key: str | None = None):
         """Initializes the Cohere client.
@@ -70,61 +66,43 @@ class CohereEncoder(DenseEncoder, AsymmetricDenseMixin):
             raise ValueError("Cohere API key cannot be 'None'.")
         return None, None
 
-    def __call__(self, docs: list[str]) -> list[list[float]]:
-        """Embed a list of documents. Supports text only.
-
-        :param docs: The documents to embed.
-        :type docs: List[str]
-        :return: The vector embeddings of the documents.
-        :rtype: List[List[float]]
-        """
-        return self.encode_queries(docs)
-
-    async def acall(self, docs: list[Any]) -> list[list[float]]:
-        """Embed a list of documents asynchronously. Supports text only.
-
-        :param docs: The documents to embed.
-        :type docs: List[str]
-        :return: The vector embeddings of the documents.
-        :rtype: List[List[float]]
-        """
-        return await self.aencode_queries(docs)
-
-    def encode_queries(self, docs: list[str]) -> list[list[float]]:
+    def encode_queries(self, docs: list[str], **kwargs) -> list[list[float]]:
         try:
             embeds = litellm.embedding(
-                input=docs, input_type="search_query", model=f"{self.type}/{self.name}"
+                input=docs, input_type="search_query", model=f"{self.type}/{self.name}", **kwargs
             )
             return litellm_to_list(embeds)
         except Exception as e:
             raise ValueError(f"Cohere API call failed. Error: {e}") from e
 
-    def encode_documents(self, docs: list[str]) -> list[list[float]]:
+    def encode_documents(self, docs: list[str], **kwargs) -> list[list[float]]:
         try:
             embeds = litellm.embedding(
                 input=docs,
                 input_type="search_document",
                 model=f"{self.type}/{self.name}",
+                **kwargs,
             )
             return litellm_to_list(embeds)
         except Exception as e:
             raise ValueError(f"Cohere API call failed. Error: {e}") from e
 
-    async def aencode_queries(self, docs: list[str]) -> list[list[float]]:
+    async def aencode_queries(self, docs: list[str], **kwargs) -> list[list[float]]:
         try:
             embeds = await litellm.aembedding(
-                input=docs, input_type="search_query", model=f"{self.type}/{self.name}"
+                input=docs, input_type="search_query", model=f"{self.type}/{self.name}", **kwargs
             )
             return litellm_to_list(embeds)
         except Exception as e:
             raise ValueError(f"Cohere API call failed. Error: {e}") from e
 
-    async def aencode_documents(self, docs: list[str]) -> list[list[float]]:
+    async def aencode_documents(self, docs: list[str], **kwargs) -> list[list[float]]:
         try:
             embeds = await litellm.aembedding(
                 input=docs,
                 input_type="search_document",
                 model=f"{self.type}/{self.name}",
+                **kwargs,
             )
             return litellm_to_list(embeds)
         except Exception as e:
--- semantic_router/encoders/mistral.py
+++ semantic_router/encoders/mistral.py
@@ -7,49 +7,48 @@ import litellm
 from pydantic import PrivateAttr
 from typing_extensions import deprecated
 
-from semantic_router.encoders import DenseEncoder
-from semantic_router.encoders.litellm import litellm_to_list
+from semantic_router.encoders.litellm import LiteLLMEncoder
 from semantic_router.utils.defaults import EncoderDefault
 
 
-class MistralEncoder(DenseEncoder):
+class MistralEncoder(LiteLLMEncoder):
     """Class to encode text using MistralAI. Requires a MistralAI API key from
     https://console.mistral.ai/api-keys/"""
 
-    _client: Any = PrivateAttr()  # TODO JB: deprecated, to remove
-    _mistralai: Any = PrivateAttr()  # TODO JB: deprecated, to remove
+    _client: Any = PrivateAttr()  # TODO: deprecated, to remove in v0.2.0
+    _mistralai: Any = PrivateAttr()  # TODO: deprecated, to remove in v0.2.0
     type: str = "mistral"
 
     def __init__(
         self,
         name: str | None = None,
         mistralai_api_key: str | None = None,
-        score_threshold: float = 0.82,
+        score_threshold: float = 0.4,
     ):
         """Initialize the MistralEncoder.
 
-        :param name: The name of the embedding model to use.
+        :param name: The name of the embedding model to use such as "mistral-embed".
         :type name: str
         :param mistralai_api_key: The MistralAI API key.
         :type mistralai_api_key: str
         :param score_threshold: The score threshold for the embeddings.
         """
+        # get default model name if none provided and convert to litellm format
         if name is None:
-            name = EncoderDefault.MISTRAL.value["embedding_model"]
-        super().__init__(
-            name=name,
-            score_threshold=score_threshold,
-        )
+            name = f"mistral/{EncoderDefault.MISTRAL.value['embedding_model']}"
+        elif not name.startswith("mistral/"):
+            name = f"mistral/{name}"
         if mistralai_api_key is None:
             mistralai_api_key = os.getenv("MISTRALAI_API_KEY")
         if mistralai_api_key is None:
             mistralai_api_key = os.getenv("MISTRAL_API_KEY")
-        if mistralai_api_key is None:
-            raise ValueError(
-                "MistralAI API key must be provided via `mistralai_api_key` parameter or "
-                "`MISTRALAI_API_KEY` environment variable."
-            )
+        super().__init__(
+            name=name,
+            score_threshold=score_threshold,
+            api_key=mistralai_api_key,
+        )
 
+    # TODO: deprecated, to remove in v0.2.0
     @deprecated("_initialize_client method no longer required")
     def _initialize_client(self, api_key):
         """Initialize the MistralAI client.
@@ -65,55 +64,3 @@ class MistralEncoder(DenseEncoder):
         if api_key is None:
             raise ValueError("Mistral API key not provided")
         return None
-
-    def __call__(self, docs: list[str]) -> list[list[float]]:
-        """Embed a list of documents. Supports text only.
-
-        :param docs: The documents to embed.
-        :type docs: List[str]
-        :return: The vector embeddings of the documents.
-        :rtype: List[List[float]]
-        """
-        return self.encode_queries(docs)
-
-    async def acall(self, docs: list[str]) -> list[list[float]]:
-        """Embed a list of documents asynchronously. Supports text only.
-
-        :param docs: The documents to embed.
-        :type docs: List[str]
-        :return: The vector embeddings of the documents.
-        :rtype: List[List[float]]
-        """
-        return await self.aencode_queries(docs)
-
-    def encode_queries(self, docs: list[str]) -> list[list[float]]:
-        try:
-            embeds = litellm.embedding(input=docs, model=f"{self.type}/{self.name}")
-            return litellm_to_list(embeds)
-        except Exception as e:
-            raise ValueError(f"Mistral API call failed. Error: {e}") from e
-
-    def encode_documents(self, docs: list[str]) -> list[list[float]]:
-        try:
-            embeds = litellm.embedding(input=docs, model=f"{self.type}/{self.name}")
-            return litellm_to_list(embeds)
-        except Exception as e:
-            raise ValueError(f"Mistral API call failed. Error: {e}") from e
-
-    async def aencode_queries(self, docs: list[str]) -> list[list[float]]:
-        try:
-            embeds = await litellm.aembedding(
-                input=docs, model=f"{self.type}/{self.name}"
-            )
-            return litellm_to_list(embeds)
-        except Exception as e:
-            raise ValueError(f"Mistral API call failed. Error: {e}") from e
-
-    async def aencode_documents(self, docs: list[str]) -> list[list[float]]:
-        try:
-            embeds = await litellm.aembedding(
-                input=docs, model=f"{self.type}/{self.name}"
-            )
-            return litellm_to_list(embeds)
-        except Exception as e:
-            raise ValueError(f"Mistral API call failed. Error: {e}") from e
