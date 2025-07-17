f51e0551f433427637441afb59ea5a963bda8815
Joshua Briggs <joshuabriggs0001@gmail.com>
2025-07-17 09:29:44 +0100

    feat: adding sparse support

---
+++ tests/unit/encoders/test_sparse_sentence_transformer.py
@@ -0,0 +1,15 @@
+import pytest
+
+from semantic_router.encoders import SparseSentenceTransformerEncoder
+from semantic_router.schema import SparseEmbedding
+
+_ = pytest.importorskip("sentence_transformers")
+
+class TestSparseSentenceTransformerEncoder:
+    def test_sparse_sentence_transformer_encoder(self):
+        encoder = SparseSentenceTransformerEncoder()
+        test_docs = ["This is a test", "This is another test"]
+        embeddings = encoder(test_docs)
+        assert isinstance(embeddings, list)
+        assert len(embeddings) == len(test_docs)
+        assert all(isinstance(embedding, SparseEmbedding) for embedding in embeddings) 
