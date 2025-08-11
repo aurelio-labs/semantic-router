bfc48dc773c5856b42ff37904c91303775e2da8c
Joshua Briggs <joshuabriggs0001@gmail.com>
2025-08-11 02:06:20 +0100

    removed any local emulator stuff and removed prints

--- .dagger/src/semantic_router_ci/main.py
+++ .dagger/src/semantic_router_ci/main.py
@@ -3,8 +3,6 @@ import os
 import dagger
 from dagger import Container, dag, function, object_type
 
-print("DAGGER DEBUG: PINECONE_API_BASE_URL =", os.environ.get("PINECONE_API_BASE_URL"))
-
 
 @object_type
 class SemanticRouter:
@@ -186,9 +184,5 @@ class SemanticRouter:
         container = container.with_env_variable(
             "POSTGRES_PASSWORD", os.environ.get("POSTGRES_PASSWORD", "postgres")
         )
-        # Debug: print PINECONE_API_BASE_URL inside the container
-        container = container.with_exec(["printenv", "PINECONE_API_BASE_URL"])
-        # Debug: print env vars inside the container
-        container = container.with_exec(["env"])
         container = container.with_exec(pytest_args)
         return await container.stdout()
